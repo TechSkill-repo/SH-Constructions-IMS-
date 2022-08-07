8bde7d1e3499d6da634451ea1ef61a35e0efd5ed
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-07 16:31:24 +0530

    starter File

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express(); //Create new instance
+
+const PORT = process.env.PORT || 9000;
+app.use(express.json()); //allows us to access request body as req.body
+app.use(morgan("dev")); //enable incoming request logging in dev mode
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
