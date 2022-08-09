73de68fac4b08d8a707249038d4e497a9449a0f2
Shahilcoder <shahilcoder@gmail.com>
2022-08-09 20:02:35 +0530

    add login, signup features

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const userControllers = require('../controllers/user.controllers');
+
+router.post('/signup', userControllers.addUser);
+router.post('/login', userControllers.getUser);
+
+module.exports = router;
