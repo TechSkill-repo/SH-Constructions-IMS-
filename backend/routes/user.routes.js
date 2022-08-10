a775d4e14a2fa5165c093c12d2c615f4db07c67d
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-10 13:16:31 +0530

    Revert "Signupb (#1)"
    This reverts commit 7e2b7061ca1fa9a8807e5ab92080313b75c36d30.

--- backend/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const userControllers = require('../controllers/user.controllers');
-
-router.post('/signup', userControllers.addUser);
-router.post('/login', userControllers.getUser);
-
-module.exports = router;
