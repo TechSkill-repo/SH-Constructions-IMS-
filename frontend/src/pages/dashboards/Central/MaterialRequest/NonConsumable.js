681efce52104abe6b43cb2b93e4a951b965d1d59
Shahilcoder <shahilcoder@gmail.com>
2022-08-22 21:07:58 +0530

    non consumable

--- frontend/src/pages/dashboards/Central/MaterialRequest/NonConsumable.js
+++ frontend/src/pages/dashboards/Central/MaterialRequest/NonConsumable.js
@@ -31,7 +31,7 @@ function NonConsumable() {
 
       <Grid container spacing={6}>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/125">
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/EC01">
             <Stats
               title="EC01"
               amount="Coke Plant"
@@ -42,107 +42,129 @@ function NonConsumable() {
           </Link>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E22"
-            amount="RMM"
-            chip="Site Store"
-            percentageText="E22"
-            percentagecolor={red[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E22">
+            <Stats
+              title="E22"
+              amount="RMM"
+              chip="Site Store"
+              percentageText="E22"
+              percentagecolor={red[500]}
+            />
+          </Link>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E13"
-            amount="RMBB"
-            chip="Site Store"
-            percentageText="E13"
-            percentagecolor={green[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E13">
+            <Stats
+              title="E13"
+              amount="RMBB"
+              chip="Site Store"
+              percentageText="E13"
+              percentagecolor={green[500]}
+            />
+          </Link>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E17"
-            amount="RMBB2"
-            chip="Site Store"
-            percentageText="E17"
-            percentagecolor={red[500]}
-          />
-        </Grid>
-      </Grid>
-      <Grid container spacing={6}>
-        <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E27"
-            amount="GBF"
-            chip="Site Store"
-            percentageText="E27"
-            percentagecolor={green[500]}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E15"
-            amount="SP#3,4"
-            chip="Site Store"
-            percentageText="E15"
-            percentagecolor={red[500]}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E23"
-            amount="SP#1,2"
-            chip="Site Store"
-            percentageText="E23"
-            percentagecolor={green[500]}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E24"
-            amount="MM"
-            chip="Site Store"
-            percentageText="E25"
-            percentagecolor={red[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E17">
+            <Stats
+              title="E17"
+              amount="RMBB2"
+              chip="Site Store"
+              percentageText="E17"
+              percentagecolor={red[500]}
+            />
+          </Link>
         </Grid>
       </Grid>
       <Grid container spacing={6}>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E20"
-            amount="LD#01"
-            chip="Site Store"
-            percentageText="E20"
-            percentagecolor={green[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E27">
+            <Stats
+              title="E27"
+              amount="GBF"
+              chip="Site Store"
+              percentageText="E27"
+              percentagecolor={green[500]}
+            />
+          </Link>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E20"
-            amount="PALLET PLANT"
-            chip="Site Store"
-            percentageText="E30"
-            percentagecolor={red[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E15">
+            <Stats
+              title="E15"
+              amount="SP#3,4"
+              chip="Site Store"
+              percentageText="E15"
+              percentagecolor={red[500]}
+            />
+          </Link>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="E28"
-            amount="MRSPP"
-            chip="Site Store"
-            percentageText="E28"
-            percentagecolor={green[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E23">
+            <Stats
+              title="E23"
+              amount="SP#1,2"
+              chip="Site Store"
+              percentageText="E23"
+              percentagecolor={green[500]}
+            />
+          </Link>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          <Stats
-            title="I11"
-            amount="LINE PLANT"
-            chip="Site Store"
-            percentageText=""
-            percentagecolor={red[500]}
-          />
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E24">
+            <Stats
+              title="E24"
+              amount="MM"
+              chip="Site Store"
+              percentageText="E25"
+              percentagecolor={red[500]}
+            />
+          </Link>
+        </Grid>
+      </Grid>
+      <Grid container spacing={6}>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E20">
+            <Stats
+              title="E20"
+              amount="LD#01"
+              chip="Site Store"
+              percentageText="E20"
+              percentagecolor={green[500]}
+            />
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E30">
+            <Stats
+              title="E30"
+              amount="PALLET PLANT"
+              chip="Site Store"
+              percentageText="E30"
+              percentagecolor={red[500]}
+            />
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/E28">
+            <Stats
+              title="E28"
+              amount="MRSPP"
+              chip="Site Store"
+              percentageText="E28"
+              percentagecolor={green[500]}
+            />
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Link style={{ textDecoration: "none" }} to="/non-consumables-table/I11">
+            <Stats
+              title="I11"
+              amount="LINE PLANT"
+              chip="Site Store"
+              percentageText=""
+              percentagecolor={red[500]}
+            />
+          </Link>
         </Grid>
       </Grid>
     </div>
