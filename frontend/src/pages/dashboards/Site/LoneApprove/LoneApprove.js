a0a07d703d259d3c936829bd55cd996c4bc729e8
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-27 14:25:09 +0530

    Store Route Changes

---
+++ frontend/src/pages/dashboards/Site/LoneApprove/LoneApprove.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoneApprove() {
+  return <div>LoneApprove</div>;
+}
+
+export default LoneApprove;
---
+++ frontend/src/pages/dashboards/Site/LoneRequest/LoneRequest.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoneRequest() {
+  return <div>LoneRequest</div>;
+}
+
+export default LoneRequest;
