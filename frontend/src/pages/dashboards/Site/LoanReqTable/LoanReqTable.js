07527f4ba50fa419530f962eac664e91a90c78a0
Shahilcoder <shahilcoder@gmail.com>
2022-08-28 11:26:13 +0530

    api integration

--- frontend/src/pages/dashboards/Site/LoanReqTable/LoanReqTable.js
+++ frontend/src/pages/dashboards/Site/LoanReqTable/LoanReqTable.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getLoans } from "../../../../service/loanService";
 
 function LoanReqTable() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getLoans(storeId)
+      .then((resp) => {
+        setItems(resp.items);
+      }).catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return <div>LoanReqTable</div>;
 }
 
