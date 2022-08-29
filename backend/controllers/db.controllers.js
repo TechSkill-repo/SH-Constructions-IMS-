75afd5be5705d4cd59bb30733907b5a58c9732dc
Shahilcoder <shahilcoder@gmail.com>
2022-08-29 12:12:57 +0530

    deploy configs

--- backend/controllers/db.controllers.js
+++ backend/controllers/db.controllers.js
@@ -8,7 +8,8 @@ const {
   Timestamp,
   FieldValue,
 } = require("firebase-admin/firestore");
-const serviceAccount = require("./serviceKey.json");
+// const serviceAccount = require("./serviceKey.json");
+const serviceAccount = JSON.parse(process.env.SERVICE_KEY);
 
 initializeApp({
   credential: cert(serviceAccount),
