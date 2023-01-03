dd0513c0574de5f546af0b15e590247afe1572ee
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2023-01-03 13:53:05 +0530

    HOST change

--- src/environments/env.js
+++ src/environments/env.js
@@ -1,2 +1,2 @@
-export const HOST = "http://localhost:9090";
-// export const HOST = "https://imsbackendserver.azurewebsites.net";
+// export const HOST = "http://localhost:9090";
+export const HOST = "https://imsbackendserver.azurewebsites.net";
