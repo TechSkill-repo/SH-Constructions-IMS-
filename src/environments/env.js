34b8eaa0ca78c2ef3851b927f3090981604e2af5
Mafiz <mafizonly@gmail.com>
2023-06-22 22:57:05 +0530

    .env host edited with new Server URL

--- src/environments/env.js
+++ src/environments/env.js
@@ -1,2 +1,4 @@
 // export const HOST = "http://localhost:9090";
-export const HOST = "https://imsbackendserver.azurewebsites.net";
+// export const HOST = "https://imsbackendserver.azurewebsites.net";
+
+export const HOST = "https://shc-ims.azurewebsites.net";
